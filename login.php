8ddb5ad70c21d6c50eed09ab73022df11ab3ba2b
sirharis214 <36185908+sirharis214@users.noreply.github.com>
2019-03-05 00:03:08 -0500

    Add files via upload
    adding skeleton of login.php from GUI from local machine for VM access

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Tab name-->
+	<title>Login Page</title>
+
+	<!--Bootstrap style sheet -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!--Personal style sheet for this page-->
+	<link rel="stylesheet" type="text/css" href="style.css" >
+
+</head>
+<body>
+
+	<div class = "container">
+		<div class="row">
+			<div class="row-md-6">
+				<h2>Login Section</h2>
+				<form action="validate.php" method="POST">
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" name="username" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="password" class="form-control" required>
+					</div>
+					<button type="submit" class="btn btn-primary">Login</button>
+				</form>
+			</div>
+			<div class="row-md-6">
+				<h2>Registration Section</h2>
+			</div>	
+		</div>
+	</div>
+
+</body>
+</html>
