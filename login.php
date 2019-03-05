29b3c97bafe053fe8a6ebad3038bfd4290d8f9cb
sirharis214 <36185908+sirharis214@users.noreply.github.com>
2019-03-05 00:48:40 -0500

    Add files via upload
    adding new styling to style sheet, updated login.php to form and also providing background image for form

--- login.php
+++ login.php
@@ -14,24 +14,37 @@
 <body>
 
 	<div class = "container">
-		<div class="row">
-			<div class="row-md-6">
-				<h2>Login Section</h2>
-				<form action="validate.php" method="POST">
-					<div class="form-group">
-						<label>Username</label>
-						<input type="text" name="username" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label>Password</label>
-						<input type="password" name="password" class="form-control" required>
-					</div>
-					<button type="submit" class="btn btn-primary">Login</button>
-				</form>
+		<div class="login-box">
+			<div class="row">
+				<div class="row-md-6 login-left">
+					<h2>Login Section</h2>
+					<form action="validate.php" method="POST">
+						<div class="form-group">
+							<label>Username</label>
+							<input type="text" name="username" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Password</label>
+							<input type="password" name="password" class="form-control" required>
+						</div>
+						<button type="submit" class="btn btn-primary">Login</button>
+					</form>
+				</div>
+				<div class="row-md-6 login-right">
+					<h2>Registration Section</h2>
+					<form action="register.php" method="POST">
+						<div class="form-group">
+							<label>Username</label>
+							<input type="text" name="username" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Password</label>
+							<input type="password" name="password" class="form-control" required>
+						</div>
+						<button type="submit" class="btn btn-primary">Register</button>
+					</form>
+				</div>	
 			</div>
-			<div class="row-md-6">
-				<h2>Registration Section</h2>
-			</div>	
 		</div>
 	</div>
 
